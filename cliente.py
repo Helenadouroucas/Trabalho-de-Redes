0c4ded731a4c974101b6c57d4e7aa1e768fda347
Victor Trambaioli <97993365+trambaioli2@users.noreply.github.com>
2023-11-01 20:19:06 -0300

    Add files via upload

---
+++ cliente.py
@@ -0,0 +1,65 @@
+import socket
+import threading
+'''                                  ***IMPORTANTE***                                  '''
+def testarip(ip,msg):           #Função para testar se a mensagem que o servidor está te retornando é a mesma...
+   lista = msg.split("[(")      #...a sua própria, evitando dobrar sua mensagem no chat...
+   lista2 = lista[1].split(",") #Se testar servidores e clientes no mesmo ip, desativar essa função
+   ip2 = lista2[0][1:-1]
+   if ip == ip2:
+      return False
+   else:
+      return True
+
+# Conectar no servidor
+conection_sucesseful = False
+ip = ''
+try:
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(('localhost', 18000))
+    conection_sucesseful = True
+    ip = client.getpeername()[0]
+    print("Você está conectado! Yay")
+    print("A mensagem de desconexão é ':D' ")  
+    print("Escreva a sua mensagem:")    
+except:
+    print("Não foi possível realizar a conexão")
+    conection_sucesseful = False
+
+encerraThread = threading.Event()
+
+# Ouvir o servidor
+def receive():
+    while not encerraThread.is_set():
+        try:
+            message = client.recv(1024).decode('utf-8')
+            if testarip(ip,message): #se testar clientes e servidores no mesmo ip, desativar esse if
+                print(message)
+
+        except ConnectionAbortedError:
+            print('ConexÃ£o Encerrada')
+            encerraThread.set()
+            
+        except:    
+            print("Um erro aconteceu!")
+            client.close()
+            encerraThread.set()
+            
+
+# Enviar pro servidor
+def write():
+    while not encerraThread.is_set():
+        message = str(input())
+        client.send(message.encode('utf-8'))
+
+        if message == ':D':
+            client.close()
+            encerraThread.set()
+            
+
+
+
+receive_thread = threading.Thread(target=receive)
+receive_thread.start()
+
+write_thread = threading.Thread(target=write)
+write_thread.start()
---
+++ servidor_final.py
@@ -0,0 +1,66 @@
+import socket
+import threading
+import os
+import time
+
+PORTA = 18000
+SERVER = 'localhost'
+ADDR = (SERVER, PORTA)
+HEADER = 64
+FORMAT = 'utf-8'
+DISCONNECT = ':D'
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
+
+# Lista para armazenar todas as conexões
+conexoes = []
+
+def handler(conn, addr):
+    try:
+        print(f'[NOVA CONEXÃO]: {addr} se conectou!')
+
+        conectado = True
+        # Adiciona a nova conexão à lista
+        conexoes.append(conn)
+        
+        while conectado:
+            #msg que eu recebo
+            msg = conn.recv(HEADER).decode(FORMAT)
+            #se a msg existe
+            if msg:
+                #vejo o tamanho
+                msg_length = len(msg)
+                if msg == DISCONNECT:
+                    conectado = False
+                    print(f"Desconctando: {addr}")
+                    continue
+
+                print(f'[{time.ctime()}][{addr}]: {msg}')
+                
+                # Envia a mensagem para todos os clientes
+                for conexao in conexoes:
+                    conexao.send(f'[{time.ctime()}][{addr}]: {msg}'.encode(FORMAT))
+
+    except Exception as e:
+        print(f"Erro: {e}")
+    finally:
+        # Remove a conexão da lista após desconectar
+        if conn in conexoes:
+            conexoes.remove(conn)
+        conn.close()
+
+
+def start():
+    server.listen()
+    print(f'O servidor está no endereço {SERVER}')
+    
+    while True:
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handler, args=(conn, addr))
+        thread.start()
+        print(f'Conexões ativas: {threading.active_count() - 1}')
+
+print('Iniciando o servidor ...')
+print('--------------------------')
+start()
